40dc13cba31d268f5d200b30097ea76598bc959c
itayshwartz1 <71848366+itayshwartz1@users.noreply.github.com>
2021-11-25 20:49:23 +0200

    Update run_main.s

--- run_main.s
+++ run_main.s
@@ -3,66 +3,85 @@
 format_char:  .string   " %c"
 format_str:   .string   " %s"
 format_int:   .string   " %d"
+pstrlen_sentence: .string "first pstring length: %d, second pstring length: %d\n"
 
 #jump table:
 .align 8        #align address to multiple of 8
 
 .JUMP_TABLE:
-    .quad .pstrlen      #case 50
-    .quad .default      #case 51
-    .quad .replaceChar  #case 52
-    .quad .pstrijcpy    #case 53
-    .quad .swapCase     #case 54
-    .quad .pstrijcmp    #case 55
-    .quad .default      #case 56
-    .quad .default      #case 57
-    .quad .default      #case 58
-    .quad .default      #case 59
-    .quad .pstrlen      #case 60
-    .quad .default      #default
+    .quad .L_pstrlen      #case 50
+    .quad .L_default      #case 51
+    .quad .L_replaceChar  #case 52
+    .quad .L_pstrijcpy    #case 53
+    .quad .L_swapCase     #case 54
+    .quad .L_pstrijcmp    #case 55
+    .quad .L_default      #case 56
+    .quad .L_default      #case 57
+    .quad .L_default      #case 58
+    .quad .L_default      #case 59
+    .quad .L_pstrlen      #case 60
+    .quad .L_default      #default
 
 .text
+
+.globl pstrlen
+.type pstrlen, @function
+pstrlen:
+    xorq    %rax, %rax
+    movb    (%rdi), %al
+    ret
+    
+
 
 
 .globl run_func
 .type run_func, @function
 run_func:
-    #setup the function
-    movq    %rsp, %rbp 
-    push    %rbp   
-     
     subq    $50, %rdx
     cmpq    $10, %rdx
-    ja .default
+    ja .L_default
     jmp *.JUMP_TABLE(,%rdx,8)
     
 .finished:                                                         
-    movq    %rbp, %rsp
-    pop     %rbp
     ret
     
-.pstrlen:
-    movq %rdi, %rsi
-    .finished
-.replaceChar:
-    movq %rdi, %rsi
-    .finished
-
-.pstrijcpy:
-    movq %rdi, %rsi
-    .finished
+.L_pstrlen:
+    call pstrlen
+    movq %rax, %r8  #save the size of pstring1 in %r8
+    movq %rsi, %rdi #swap that now %rdi point to pstring2
+    call pstrlen
+    movq %rax, %r9  #save the size of pstring1 in %r9
     
-.swapCase:
-    movq %rdi, %rsi
-    .finished
+    #printing
+    movq $pstrlen_sentence, %rdi
+    movq %r8, %rsi
+    movq %r9, %rdx
+    xorq %rax, %rax
+    call printf
+    
 
-.pstrijcmp:
+    
+    jmp .finished
+    
+.L_replaceChar:
     movq %rdi, %rsi
-    .finished
+    jmp .finished
 
-.default:
+.L_pstrijcpy:
     movq %rdi, %rsi
-    .finished
+    jmp .finished
+    
+.L_swapCase:
+    movq %rdi, %rsi
+    jmp .finished
+
+.L_pstrijcmp:
+    movq %rdi, %rsi
+    jmp .finished
+
+.L_default:
+    movq %rdi, %rsi
+    jmp .finished
 
 
 
