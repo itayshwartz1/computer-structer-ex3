49dc3583e354347b90f395d35adb5057565e5ad0
itayshwartz1 <71848366+itayshwartz1@users.noreply.github.com>
2021-11-25 21:57:22 +0200

    Update run_main.s

--- run_main.s
+++ run_main.s
@@ -27,9 +27,17 @@ pstrlen_sentence: .string "first pstring length: %d, second pstring length: %d\n
 .globl pstrlen
 .type pstrlen, @function
 pstrlen:
-    xorq    %rax, %rax
-    movb    (%rdi), %al
+    xorq    %rax, %rax  #we make sure that %rax is 0
+    movb    (%rdi), %al #return the size of 
     ret
+    
+.globl replaceChar
+.type replaceChar, @function
+replaceChar:
+    xorq %rax, %rax
+    ret
+
+  
     
 
 
@@ -37,35 +45,63 @@ pstrlen:
 .globl run_func
 .type run_func, @function
 run_func:
-    subq    $50, %rdx
-    cmpq    $10, %rdx
-    ja .L_default
-    jmp *.JUMP_TABLE(,%rdx,8)
+    subq    $50, %rdx                   # we arrange the input so that for 52 for example we get 2
+    cmpq    $10, %rdx                   #we check if the choice is outside the range
+    ja      .L_default                  #if it outside the range we jump to default
+    jmp     *.JUMP_TABLE(,%rdx,8)       #else we go to the jump tanle and jump to the relevant label
     
-.finished:                                                         
+.finished:                              #we return from the function                                                         
     ret
     
 .L_pstrlen:
-    call pstrlen
-    movq %rax, %r8  #save the size of pstring1 in %r8
-    movq %rsi, %rdi #swap that now %rdi point to pstring2
-    call pstrlen
-    movq %rax, %r9  #save the size of pstring1 in %r9
+    call    pstrlen                     #calculate the len of pstring 1
+    movq    %rax, %r8                   #save the size of pstring1 in %r8
+    movq    %rsi, %rdi                  #swap that now %rdi point to pstring2
+    call    pstrlen                     #calculate the len of pstring 2
+    movq    %rax, %r9                   #save the size of pstring1 in %r9
     
     #printing
-    movq $pstrlen_sentence, %rdi
-    movq %r8, %rsi
+    movq $pstrlen_sentence, %rdi        #we put the format in the first argument (%rdi) for printing
+    movq %r8, %rsi                      
     movq %r9, %rdx
     xorq %rax, %rax
     call printf
-    
-
-    
+    ret
     jmp .finished
+    
     
 .L_replaceChar:
-    movq %rdi, %rsi
+    push    %rbp            #save %rbp       
+    movq    %rsp, %rbp      #bring rbp to the start of this frame
+    sub     $16, %rsp       #make space to the letters that we scan in the stack
+    
+    movq    %rdi, %r8       #save the pointer to pstring1 in %r8
+    movq    %rdi, %r9       #save the pointer to pstring2 in %r9
+    
+    #get the first letter 
+    movq     $format_char, %rdi #give the format to scanf on first argument
+    leaq    -16(%rbp), %rsi     #give to scanf the adreess thet he save in the letter
+    xorq    %rax, %rax          #put 0 in %rax
+    call    scanf
+    
+    #get the second letter 
+    movq     $format_char, %rdi #give the format to scanf on first argument
+    leaq    -8(%rbp), %rsi      #give to scanf the adreess thet he save in the letter
+    xorq    %rax, %rax          #put 0 in %rax
+    call    scanf
+    
+    movq      %r8, %rdi         # the first argument to replaceChar is *pstring1
+    movzbq    -16(%rbp), %rsi   #the second is the first letter
+    movzbq    -8(%rbp), %rdx    #the third is the second letter
+    call replaceChar #call replaceChar
+    
+    popq %r8
+    popq %r8
+    movq    %rbp, %rsp      #return #rsp to the start of the frame
+    pop     %rbp            #retun the %rbp to the address that he was before
     jmp .finished
+
+
 
 .L_pstrijcpy:
     movq %rdi, %rsi
